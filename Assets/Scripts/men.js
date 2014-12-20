95c8fb7cf57fabeebf24f5c15040bd4e52e46a7e
PeterWu1990 <peter4172002@hotmail.com>
2014-12-20 12:07:26 +0800

    跑酷場景人物跳躍

---
+++ Assets/Scripts/men.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+
+var speed : float = 5.0f;
+var jumpForce : float = 300.0f;
+
+function Start () {
+
+}
+
+function Update () {
+	rigidbody2D.velocity = new Vector2(speed, rigidbody2D.velocity.y);
+	
+	if(Input.GetButtonDown("Jump")){
+		rigidbody2D.AddForce(new Vector2(0,jumpForce));
+	}
+}
